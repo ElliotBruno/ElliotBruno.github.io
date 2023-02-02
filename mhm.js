b3a23f9abdabe55d26b0a8ff089799c8948ff34c
Elliot <elliot.bruno@skola.taby.se>
2023-02-02 13:56:17 +0100

    d

--- mhm.js
+++ mhm.js
@@ -2,4 +2,10 @@ let a="Elliot"
 console.log(a)
 var kongo = document.getElementById('kongo');
 kongo.innerText="Tjo"
+kongo.classList.add()
 
+
+document.getElementById("kongo").classList.add("change-bg");
+kongo.insertAdjacentHTML("beforebegin", "<h1 id='kongo' class='change-bg'>Javascript</h1>");
+let heading= document.getElementById("rurik")
+heading.insertAdjacentElement("afterend", "<p>Hola</p>")
